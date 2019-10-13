dc1016cf38ac3faecfdfd9e6dd7fd5ae9e42b0fe
wucangeo <wucangeo@gmail.com>
2019-10-13 23:56:54 +0800

    首次上传

---
+++ app/models/gift.py
@@ -0,0 +1,18 @@
+"""
+ Created by 七月 on 2018/5/7.
+ 微信公众号：林间有风
+"""
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.models.base import Base
+
+__author__ = '七月'
+
+
+class Gift(Base):
+    id = Column(Integer, primary_key=True)
+    user = relationship('User')
+    uid = Column(Integer, ForeignKey('user.id'))
+    isbn = Column(String(15), nullable=False)
+    launched = Column(Boolean, default=False)
